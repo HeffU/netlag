86a85fd4d49e10386c6549b18cdc0425cfe5a195
Heff <heff@heff.se>
2014-11-26 19:44:38 +0100

    CMake project structuring and library fixes

---
+++ src/netlag.cpp
@@ -0,0 +1,32 @@
+/**********************************************************************
+NetLag - Shenanigans in Virtual Reality
+Copyright(C) 2014 Rasmus Egerö <heff@heff.se>
+
+This program is free software : you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see <http://www.gnu.org/licenses/
+***********************************************************************
+*	netlag.cpp - Entry point and bootstrapping
+**********************************************************************/
+
+#include "engine\engine.h"
+using namespace NetLag;
+
+int main(int argc, char *argv[])
+{
+	Engine* engine = new Engine();
+	engine->Initialize();
+	engine->Run();
+	engine->Cleanup();
+
+	return 0;
+}
